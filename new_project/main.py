862451fa6b731ad2a1de8dbff968b2ca06984968
olehandro <odpash.itmo@gmail.com>
2023-09-22 23:16:31 +0300

    a

--- new_project/main.py
+++ new_project/main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 async def main(username: str, request: Request):
     print(username, request.json())
     return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
