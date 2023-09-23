5d8d6e7e160b0dd3cf259e842c6230700dd109dd
olehandro <odpash.itmo@gmail.com>
2023-09-23 19:18:48 +0300

    a

--- new_project/main.py
+++ new_project/main.py
@@ -10,28 +10,6 @@ UPDATE_PIPELINE_KEY = 'leads[update][0][pipeline_id]'
 
 
 class PostDataHandler(tornado.web.RequestHandler):
-    async def _get_request_dict(self):
-        decoded_data = unquote(self.request.body.decode('utf-8')).split('&')
-        request_dict = {}
-        for el in decoded_data:
-            params = el.split('=')
-            k, v = params[0], params[1]
-            request_dict[k] = v
-        return request_dict
-
-    async def _update_pipeline_information(self, r_d):
-        if NEW_CLIENT_KEY in r_d.keys():
-            lead_id, pipeline_id, status_id = r_d[f'leads[add][0][lead_id]'], r_d[f'leads[add][0][pipeline_id]'], 0
-        else:
-            lead_id, pipeline_id, status_id = r_d[f'leads[update][0][id]'], r_d[f'leads[update][0][pipeline_id]'], 0
-        result = session.query(Leads).filter_by(id=lead_id).first()
-
-        if result:
-            result.pipeline_id, result.status_id = pipeline_id, status_id
-        else:
-            new_lead = Leads(id=lead_id, pipeline_id=pipeline_id, status_id=status_id)
-            session.add(new_lead)
-        session.commit()
 
     async def post(self, username):
         print(username)
