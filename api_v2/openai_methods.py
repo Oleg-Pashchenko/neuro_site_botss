b857ab99e6fda13b501b74f56b09da10c2a9dc44
olehandro <odpash.itmo@gmail.com>
2023-10-19 03:17:02 +0300

    e

--- api_v2/openai_methods.py
+++ api_v2/openai_methods.py
@@ -60,8 +60,6 @@ def find_from_database(filename, params, rules):
     for d in list_of_arrays:
         approved = True
         for k, v in d.items():
-            if rules[k] == '<=':
-                print((int(v) >= int(params[k])))
             try:
                 if rules[k] == '=' and v != params[k]:
                     approved = False
